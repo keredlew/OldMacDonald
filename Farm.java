6d0ed6062bb0f4b355101bb073ea8f46752c3b96
keredlew <derek.lew@comcast.net>
2016-10-14 10:45:42 -0700

    changed code

--- Farm.java
+++ Farm.java
@@ -1,4 +1,18 @@
-class Farm 
+public void setup() 
 {     
-   //your code here
+    Cow c = new Cow("cow", "moo");   
+    System.out.println(c.getType() + " goes " + c.getSound());  }  
 }
+class Farm  
+{     
+   private Animal[] aBunchOfAnimals = new Animal[3];    
+   public Farm()     {       
+      aBunchOfAnimals[0] = new Cow("cow","moo");           
+      aBunchOfAnimals[1] = new Chick("chick","cluck");       
+      aBunchOfAnimals[2] = new Pig("pig","oink");    }         
+   public void animalSounds()    {       
+     for (int nI=0; nI < aBunchOfAnimals.length; nI++)       {          
+       System.out.println( aBunchOfAnimals[nI].getType() + " goes " + aBunchOfAnimals[nI].getSound());       
+      }    
+    } 
+} 
